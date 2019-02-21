078d44025710e79ed2d72b2d28e531c728e0d47d
Animesh Singh <anymeshsingh@gmail.com>
2019-02-22 00:51:37 +0530

    Deployed website

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+    <Layout>
+        <h1>About BitzPrice</h1>
+        <p>It is a simple Bitcoin rate checking site made with Next.js and React.</p>
+    </Layout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+import Prices from '../components/Prices';
+
+const Index = (props) => (
+    <Layout>
+        <h1>Welcome to BitzPrice</h1>
+        <p>Check current BitCoin rate</p>
+        <Prices bpi={props.bpi}/>
+    </Layout>
+);
+
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+}
+
+export default Index;
